a3e02930d0eb81305f7b7233ff6d8306cba9b721
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-06-02 19:17:16 +0600

    [DONE] Create User Interface and Module

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("Data base is connected successfully");
+    await mongoose.connect(config.database_url as string)
+    console.log('Data base is connected successfully')
 
     app.listen(config.port, () => {
-      console.log(`application app listening on port ${config.port}`);
-    });
+      console.log(`application app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Failled to connect databse", error);
+    console.log('Failled to connect databse', error)
   }
 }
-bootstrap();
+bootstrap()
